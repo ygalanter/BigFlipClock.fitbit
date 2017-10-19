f45bc22302582e0ef16033efb5285248331128ce
ygalanter <yuri@galanter.net>
2017-10-19 12:21:28 -0400

    Added settingd to enable/disable animation

--- app/index.js
+++ app/index.js
@@ -2,38 +2,12 @@
 import clock from "clock";
 import document from "document";
 import {display} from "display";
+import * as messaging from "messaging";
+import * as fs from "fs";
+import { me } from "appbit";
 import {preferences} from "user-settings";
 import dtlib from "../common/datetimelib"
 
-//getting handle on SVG elements for time
-let h1_top_anim_img = document.getElementById("h1_top_anim_img");
-let h1_bottom_anim_img = document.getElementById("h1_bottom_anim_img");
-let h1_top_anim = document.getElementById("h1_top_anim");
-let h1_bottom_anim = document.getElementById("h1_bottom_anim");
-let h1_top_perm_img = document.getElementById("h1_top_perm_img");
-let h1_bottom_perm_img = document.getElementById("h1_bottom_perm_img");
-
-
-let h2_top_anim_img = document.getElementById("h2_top_anim_img");
-let h2_bottom_anim_img = document.getElementById("h2_bottom_anim_img");
-let h2_top_anim = document.getElementById("h2_top_anim");
-let h2_bottom_anim = document.getElementById("h2_bottom_anim");
-let h2_top_perm_img = document.getElementById("h2_top_perm_img");
-let h2_bottom_perm_img = document.getElementById("h2_bottom_perm_img");
-
-let m1_top_anim_img = document.getElementById("m1_top_anim_img");
-let m1_bottom_anim_img = document.getElementById("m1_bottom_anim_img");
-let m1_top_anim = document.getElementById("m1_top_anim");
-let m1_bottom_anim = document.getElementById("m1_bottom_anim");
-let m1_top_perm_img = document.getElementById("m1_top_perm_img");
-let m1_bottom_perm_img = document.getElementById("m1_bottom_perm_img");
-
-let m2_top_anim_img = document.getElementById("m2_top_anim_img");
-let m2_bottom_anim_img = document.getElementById("m2_bottom_anim_img");
-let m2_top_anim = document.getElementById("m2_top_anim");
-let m2_bottom_anim = document.getElementById("m2_bottom_anim");
-let m2_top_perm_img = document.getElementById("m2_top_perm_img");
-let m2_bottom_perm_img = document.getElementById("m2_bottom_perm_img");
 
 // get handle on textboxes for date, DoW, AM/PM
 let monthlbl = document.getElementById("month");
@@ -48,11 +22,60 @@ let timeh2 = 0;
 let timem1 = 0;
 let timem2 = 0;
 
+// whether to skip time animation and switch to new time fast
+const ANIMATION_DISABLE_ALWAYS = 0
+const ANIMATION_DISABLE_ON_SCREEN_WAKEUP = 1
+const ANIMATION_ENABLE_ALWAYS = 2
+let userSettings;
+let screenJustAwoke = true;
+
+// trying to get user settings if saved before
+try {
+  userSettings = fs.readFileSync("user_settings.json", "json");
+} catch (e) {
+  userSettings = {timeAnimation: ANIMATION_DISABLE_ON_SCREEN_WAKEUP}
+}
+
+
+
 // get user time format preference
 dtlib.timeFormat = preferences.clockDisplay == "12h" ? 1: 0;
 
 // clock ticks every minute
 clock.granularity = "minutes";
+
+// change time animations
+function changeTime(position, oldTime, newTime) {
+   
+    //getting handle on SVG elements for time
+    let top_anim_img = document.getElementById(`${position}_top_anim_img`);
+    let bottom_anim_img = document.getElementById(`${position}_bottom_anim_img`);
+    let top_anim = document.getElementById(`${position}_top_anim`);
+    let bottom_anim = document.getElementById(`${position}_bottom_anim`);
+    let top_perm_img = document.getElementById(`${position}_top_perm_img`);
+    let bottom_perm_img = document.getElementById(`${position}_bottom_perm_img`);
+
+    //chanhing time
+    top_perm_img.href = `digits/${newTime}top.png`; //new
+    bottom_anim_img.href = `digits/${newTime}bottom.png`; //new
+  
+    // if animation is disabled - we're done here, exiting
+    if (userSettings.timeAnimation == ANIMATION_DISABLE_ALWAYS || (userSettings.timeAnimation == ANIMATION_DISABLE_ON_SCREEN_WAKEUP && screenJustAwoke)) {
+        return; 
+    } 
+  
+        
+    // if we got this far, it means animation is enabled - go for it!
+    top_anim_img.href = `digits/${oldTime}top.png`; //old
+    bottom_perm_img.href = `digits/${oldTime}bottom.png`; //old
+    bottom_anim_img.style.display = "none";
+    top_anim.animate = true;
+    setTimeout(function(){bottom_anim_img.style.display = "inline";bottom_anim.animate = true},1000);
+
+    
+}
+
+
 
 // clock tick
 function updateClock() {
@@ -70,61 +93,29 @@ function updateClock() {
   let m1 = Math.floor(mins/10);
   let m2 = mins % 10;
   
-  // if first digit og hours changed - animate it
+  // if first digit of hours changed - animate it
   if (timeh1 != h1) { 
-
-    h1_top_perm_img.href = `digits/${h1}top.png`; //new
-    h1_top_anim_img.href = `digits/${timeh1}top.png`; //old
-    h1_bottom_perm_img.href = `digits/${timeh1}bottom.png`; //old
-    h1_bottom_anim_img.style.display = "none";
-    h1_bottom_anim_img.href = `digits/${h1}bottom.png`; //new
-    h1_top_anim.animate = true;
-    setTimeout(function(){h1_bottom_anim_img.style.display = "inline";h1_bottom_anim.animate = true},1000);
-    
+    changeTime('h1', timeh1, h1)
     timeh1 = h1;
   }
   
   // same as H1 for second hour digit
   if (timeh2 != h2) {
-    
-    h2_top_perm_img.href = `digits/${h2}top.png`; //new
-    h2_top_anim_img.href = `digits/${timeh2}top.png`; //old
-    h2_bottom_perm_img.href = `digits/${timeh2}bottom.png`; //old
-    h2_bottom_anim_img.style.display = "none";
-    h2_bottom_anim_img.href = `digits/${h2}bottom.png`; //new
-    h2_top_anim.animate = true;
-    setTimeout(function(){h2_bottom_anim_img.style.display = "inline";h2_bottom_anim.animate = true},1000);    
-    
+    changeTime('h2', timeh2, h2);
     timeh2 = h2;
   }
   
   
   // same as h1 for first minute digit
   if (timem1 != m1) {
-    
-    m1_top_perm_img.href = `digits/${m1}top.png`; //new
-    m1_top_anim_img.href = `digits/${timem1}top.png`; //old
-    m1_bottom_perm_img.href = `digits/${timem1}bottom.png`; //old
-    m1_bottom_anim_img.style.display = "none";
-    m1_bottom_anim_img.href = `digits/${m1}bottom.png`; //new
-    m1_top_anim.animate = true;
-    setTimeout(function(){m1_bottom_anim_img.style.display = "inline";m1_bottom_anim.animate = true},1000);    
-    
+    changeTime('m1', timem1, m1)  
     timem1 = m1;
   }
   
   
   // same as h1 for second minute digit
   if (timem2 != m2) {
-    
-    m2_top_perm_img.href = `digits/${m2}top.png`; //new
-    m2_top_anim_img.href = `digits/${timem2}top.png`; //old
-    m2_bottom_perm_img.href = `digits/${timem2}bottom.png`; //old
-    m2_bottom_anim_img.style.display = "none";
-    m2_bottom_anim_img.href = `digits/${m2}bottom.png`; //new
-    m2_top_anim.animate = true;
-    setTimeout(function(){m2_bottom_anim_img.style.display = "inline";m2_bottom_anim.animate = true},1000);    
-    
+    changeTime('m2', timem2, m2)
     timem2 = m2;
   }
   
@@ -139,6 +130,9 @@ function updateClock() {
   
   // displaying AM/PM or 24H
   ampmlbl.innerText = dtlib.getAmApm(today.getHours());
+  
+  // resetting screen awoke flag
+  screenJustAwoke = false;
 } 
  
 // assigning clock tick event handler
@@ -146,3 +140,35 @@ clock.ontick = () => updateClock();
 
 // and cicking off first time change
 updateClock();
+
+
+// Message is received
+messaging.peerSocket.onmessage = evt => {
+  
+  switch (evt.data.key) {
+      case "timeDigitanimation": // if this is animation setting
+          userSettings.timeAnimation = JSON.parse(evt.data.newValue).values[0].value;
+          break;
+  };
+      
+}
+
+// Message socket opens
+messaging.peerSocket.onopen = () => {
+  console.log("App Socket Open");
+};
+
+// Message socket closes
+messaging.peerSocket.close = () => {
+  console.log("App Socket Closed");
+};
+
+// on app exit collect settings 
+me.onunload = () => {
+    fs.writeFileSync("user_settings.json", userSettings, "json");
+}
+
+// on display on/off set the flag
+display.onchange = () => {
+  screenJustAwoke = display.on;
+}
